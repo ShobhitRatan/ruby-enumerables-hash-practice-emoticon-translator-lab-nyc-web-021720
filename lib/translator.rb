a6cfd1e6696e1ef91957e83eade538b580ab7036
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-07 22:25:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
 # require modules here
+require "yaml"
+require "pry"
+require "pp"
 
-def load_library
+def load_library(library)
   # code goes here
+  emoticons = YAML.load_file(library)
+  #pp emoticons
+  emoticons
+  emote = {:get_meaning => {}, :get_emoticon => {}}
+  get_meaning =
+  emoticons.each do |key, value|
+    emote[:get_meaning][value[1]] = key
+    emote[:get_emoticon][value[0]] = value[1]
+  end
+  puts emote
+  emote 
 end
 
 def get_japanese_emoticon
